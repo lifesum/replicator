b5930234acfb782cae37864bbf8d9dab25cb8538
Erik Jansson <erik@lifesum.com>
2017-09-25 15:31:33 +0200

    Try this service instead

--- client/nomad.go
+++ client/nomad.go
@@ -299,7 +299,7 @@ func (c *nomadClient) GetJobAllocations(allocs []*nomad.AllocationListStub, gsp 
 			(allocationStub.DesiredStatus == nomadStructs.AllocDesiredStatusRun) {
 			if alloc, _, err := c.nomad.Allocations().Info(allocationStub.ID, c.queryOptions()); err == nil && alloc != nil {
 				failed := false
-				for service := range alloc.Services {
+				for nn, service := range alloc.Services {
 					logging.Debug("%s: checking map for %s", service, alloc.ID)
 					if k, ok := critical[service]; ok {
 						if v, ok := k[alloc.ID]; ok {
